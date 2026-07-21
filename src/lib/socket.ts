48f863f4feff2879327223136fb76fe255298e5a
Rocky Gonzales <gonzrock12@gmail.com>
2026-07-21 09:19:58 +0800

    update socket.ts notification

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const socket = io(
-  'http://localhost:4000', //'https://manufacturer-ecological-dat-tone.trycloudflare.com/',
+  'https://manufacturer-ecological-dat-tone.trycloudflare.com/', // 'http://localhost:4000', //
   {
     autoConnect: false,
 
