de18d6567e0591e762f633599aa366e2dbd953fc
Rocky Gonzales <gonzrock12@gmail.com>
2026-08-18 14:27:07 +0800

    update socket to live

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const socket = io(
-  'http://localhost:4000', // 'https://charles-packs-plaintiff-streams.trycloudflare.com/', //
+  'https://charles-packs-plaintiff-streams.trycloudflare.com/', //'http://localhost:4000', //
   {
     autoConnect: false,
 
