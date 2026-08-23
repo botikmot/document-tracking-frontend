60a9a4d9fa205f68d04eb99a70156aba25495de8
Rocky Gonzales <gonzrock12@gmail.com>
2026-08-23 20:09:24 +0800

    update socket.io link

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const socket = io(
-  'https://charles-packs-plaintiff-streams.trycloudflare.com/', // http://localhost:4000', //
+  'https://concentration-kitchen-reading-reproductive.trycloudflare.com/', // http://localhost:4000', //
   {
     autoConnect: false,
 
